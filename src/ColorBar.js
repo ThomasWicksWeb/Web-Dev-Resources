928d91538bf351d81e310fcb98ff6d97f2491274
ljosberinn <admin@gerritalex.de>
2020-03-25 23:33:48 +0100

    refactor(NavBar): add NavbarItem

--- src/ColorBar.js
+++ src/ColorBar.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-function ColorBar(){
-    return(
-        <div className="colorBand">&nbsp;</div>
-    )
+function ColorBar() {
+  return <div className="colorBand">&nbsp;</div>;
 }
 
 export default ColorBar;
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,46 +1,70 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const NavBar = () => {
-
-
-    return(
-        <div className="tabs is-centered">
-        <ul>
-            <li className="tab-links">
-                <NavLink to="./Colors"><span className="icon is-small"><i className="fas fa-paint-brush" aria-hidden="true"></i></span><span>Colors</span></NavLink>
-            </li>
-
-            <li className="tab-links">
-                <NavLink to="./Images"><span className="icon is-small"><i className="fas fa-images" aria-hidden="true"></i></span><span>Images</span></NavLink>
-            </li>
-
-            <li className="tab-links">
-                <NavLink to="./Backgrounds"><span className="icon is-small"><i className="fas fa-image" aria-hidden="true"></i></span><span>Backgrounds</span></NavLink>
-            </li>
-
-            <li className="tab-links">
-                <NavLink to="./Css"><span className="icon is-small"><i className="fab fa-css3-alt" aria-hidden="true"></i></span><span>CSS</span></NavLink>
-            </li>
-
-            <li className="tab-links">
-                <NavLink to="./HTML"><span className="icon is-small"><i className="fab fa-html5" aria-hidden="true"></i></span><span>HTML</span></NavLink>
-            </li>
-
-            <li className="tab-links">
-                <NavLink to="./Inspiration"><span className="icon is-small"><i className="far fa-lightbulb" aria-hidden="true"></i></span><span>Inspiration</span></NavLink>
-            </li>
-
-            <li className="tab-links">
-                <NavLink to="./Javascript"><span className="icon is-small"><i className="fab fa-js-square" aria-hidden="true"></i></span><span>Javascript</span></NavLink>
-            </li>
-            
-            <li className="tab-links">
-                <NavLink to="./Articles"><span className="icon is-small"><i className="fas fa-book" aria-hidden="true"></i></span><span>Articles</span></NavLink>
-            </li>
-        </ul>
-    </div>
-    )
+function NavbarItem({ title, icon, to }) {
+  return (
+    <li className="tab-links">
+      <NavLink to={to}>
+        <span className="icon is-small">
+          <i className={icon} aria-hidden="true"></i>
+        </span>
+        <span>{title}</span>
+      </NavLink>
+    </li>
+  );
 }
 
-export default NavBar;
+const items = [
+  {
+    title: 'Colors',
+    icon: 'fas fa-paint-brush',
+    to: '/colors',
+  },
+  {
+    title: 'Images',
+    icon: 'fas fa-images',
+    to: '/images',
+  },
+  {
+    title: 'Backgrounds',
+    icon: 'fas fa-image',
+    to: '/backgrounds',
+  },
+  {
+    title: 'CSS',
+    icon: 'fab fa-css3-alt',
+    to: '/css',
+  },
+  {
+    title: 'HTML',
+    icon: 'fab fa-html5',
+    to: '/html',
+  },
+  {
+    title: 'Inspiration',
+    icon: 'far fa-lightbulb',
+    to: '/inspiration',
+  },
+  {
+    title: 'Javascript',
+    icon: 'fab fa-js-square',
+    to: '/javascript',
+  },
+  {
+    title: 'Articles',
+    icon: 'fas fa-book',
+    to: '/articles',
+  },
+];
+
+export default function NavBar() {
+  return (
+    <div className="tabs is-centered">
+      <ul>
+        {items.map(data => (
+          <NavbarItem {...data} key={data.to} />
+        ))}
+      </ul>
+    </div>
+  );
+}
