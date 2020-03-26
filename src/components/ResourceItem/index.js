ed2d5e8f83e4c452fad304e3e0e0894cf0da92f9
ljosberinn <admin@gerritalex.de>
2020-03-26 01:24:05 +0100

    feat(html): improve semantic html in ResourceItem

--- src/components/ResourceItem/index.js
+++ src/components/ResourceItem/index.js
@@ -7,9 +7,13 @@ export default function ResourceItem({ href, img, alt, name, desc }) {
   return (
     <li className="box">
       <ExternalLink className="has-text-left" href={href}>
-        <img src={img} alt={alt} />
-        <h2 className="is-size-4">{name}</h2>
-        <p className="is-size-6">{desc}</p>
+        <figure>
+          <img src={img} alt={alt} loading="lazy" />
+          <figcaption>
+            <h2 className="is-size-4">{name}</h2>
+            <p className="is-size-6">{desc}</p>
+          </figcaption>
+        </figure>
       </ExternalLink>
     </li>
   );
@@ -20,5 +24,5 @@ ResourceItem.propTypes = {
   img: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  desc: PropTypes.string.isRequired,
+  desc: PropTypes.string,
 };
