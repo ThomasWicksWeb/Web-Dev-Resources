cc958e5da70034d604b7ee8a46e9564a8e6e4cf3
ljosberinn <admin@gerritalex.de>
2020-03-26 01:00:34 +0100

    refactor(styling): add bulma

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import 'bulma/css/bulma.min.css';
+import './assets/css/resourceStyle.css';
+
 import App from './App';
 
 render(<App />, document.getElementById('root'));
