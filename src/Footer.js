808c875aa4a712fd816a46deaa558e6899362c7a
ThomasWicksWeb <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-04-02 21:38:53 -0400

    Updated last edit date

--- src/Footer.js
+++ src/Footer.js
@@ -46,7 +46,7 @@ function AppFooter() {
           </ExternalLink>{' '}
         </p>
         <p className="is-size-6">
-          Site last updated on <strong>March 23rd, 2020</strong>
+          Site last updated on <strong>April 2nd, 2020</strong>
         </p>
       </div>
     </footer>
